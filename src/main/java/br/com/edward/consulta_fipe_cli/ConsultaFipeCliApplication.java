3e8b9b272b4011a46bcbd97db0e1a554c5e52f17
Jhonatan Edward <jhonatanedsp@gmail.com>
2025-11-07 11:29:07 -0300

    Primeiro commit, projeto iniciado

---
+++ src/main/java/br/com/edward/consulta_fipe_cli/ConsultaFipeCliApplication.java
@@ -0,0 +1,13 @@
+package br.com.edward.consulta_fipe_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaFipeCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaFipeCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/edward/consulta_fipe_cli/ConsultaFipeCliApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edward.consulta_fipe_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaFipeCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
